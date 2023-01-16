34e5a05fcaecab2880a23cf635aedd3c2e8def28
Tung Bui <tung.bq@adamo.software>
2023-01-16 08:19:53 +0700

    devise login google

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'example/redirect'
   get 'sessions/create'
   get 'sessions/destroy'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'home/index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
